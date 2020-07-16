e1cf4e0d0c72ec5ef275ff527ccdbfc4ef51045a
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-07-17 00:24:43 +0600

    error handel of problem 1

--- assignment.js
+++ assignment.js
@@ -3,12 +3,15 @@ function feetToMile(distance){
     var unit = 0.000189394; // 1 feet = 0.000189394 mile.
     if(distance<0){
         console.log("Distance can not be negetive...");
+        return 0;
     }else{
         var maile = distance * unit; // convert feet to maile
     }
     return maile;
 }
 
-var feet = 5684;
-var mile = feetToMile(feet).toFixed(4); // call function for convert
-console.log(feet + " Feet = "+mile+" Mile. \n");
+var feet = 5897;
+var mile = feetToMile(feet).toFixed(5); // call function for convert
+if(feet >= 0){
+    console.log(feet + " Feet = "+mile+" Mile. \n");
+}
